dc0a3bcbd41310a2c28db5dcffd6fd30326223b4
CatMeox <phattarapornhsr@Yossas-MacBook-Pro.local>
2023-01-26 16:55:44 +0700

    feat: add sign up screen

---
+++ iFat/iFat/View/WelcomeScreen/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  iFat
+//
+//  Created by CatMeox on 26/1/2566 BE.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
